61ca11074b083df6008890d27651f9f8c7e50b99
BordListian <bordlistian@hotmail.de>
2017-09-01 16:38:54 +0200

    - Changes Carpenter Saw to harvest sidings, mouldings and corners (needs refinement), fixes #34

--- src/main/java/betterwithaddons/item/rbdtools/ItemCarpenterSaw.java
+++ src/main/java/betterwithaddons/item/rbdtools/ItemCarpenterSaw.java
@@ -1,5 +1,6 @@
 package betterwithaddons.item.rbdtools;
 
+import betterwithmods.common.blocks.mini.BlockMini;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -43,7 +44,7 @@ public class ItemCarpenterSaw extends ItemAxeConvenient {
     }
 
     public static boolean isCarpentry(ItemStack tool, IBlockState state) {
-        return state.getMaterial() == Material.WOOD && !ItemKukri.isTree(tool, state);
+        return (state.getMaterial() == Material.WOOD || state.getMaterial() == BlockMini.MINI) && !ItemKukri.isTree(tool, state);
     }
 
 }
