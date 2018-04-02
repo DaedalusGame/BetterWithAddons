aa98715a578f1838eb7a5dac596b167fb66fe153
BordListian <bordlistian@hotmail.de>
2018-04-03 00:35:19 +0200

    - FUCK

--- src/main/java/betterwithaddons/config/ModConfiguration.java
+++ src/main/java/betterwithaddons/config/ModConfiguration.java
@@ -1,18 +1,24 @@
 package betterwithaddons.config;
 
 import betterwithaddons.interaction.ModInteractions;
+import betterwithaddons.lib.Reference;
+import betterwithmods.BWMod;
 import betterwithmods.module.ModuleLoader;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ModConfiguration {
     public static Configuration configuration;
     private static boolean needsRestart = true;
 
     public static int loadPropInt(String propName, String category, String desc, int default_, int min, int max) {
-        Property prop = ModuleLoader.config.get(category, propName, default_, desc, min, max);
+        Property prop = configuration.get(category, propName, default_, desc, min, max);
         prop.setComment(desc);
         setNeedsRestart(prop);
 
@@ -20,7 +26,7 @@ public class ModConfiguration {
     }
 
     public static int loadPropInt(String propName, String category, String desc, int default_) {
-        Property prop = ModuleLoader.config.get(category, propName, default_);
+        Property prop = configuration.get(category, propName, default_);
         prop.setComment(desc);
         setNeedsRestart(prop);
 
@@ -28,7 +34,7 @@ public class ModConfiguration {
     }
 
     public static double loadPropDouble(String propName, String category, String desc, double default_) {
-        Property prop = ModuleLoader.config.get(category, propName, default_);
+        Property prop = configuration.get(category, propName, default_);
         prop.setComment(desc);
         setNeedsRestart(prop);
 
@@ -36,7 +42,7 @@ public class ModConfiguration {
     }
 
     public static double loadPropDouble(String propName, String category, String desc, double default_, double min, double max) {
-        Property prop = ModuleLoader.config.get(category, propName, default_, desc, min, max);
+        Property prop = configuration.get(category, propName, default_, desc, min, max);
         prop.setComment(desc);
         setNeedsRestart(prop);
 
@@ -44,7 +50,7 @@ public class ModConfiguration {
     }
 
     public static boolean loadPropBool(String propName, String category, String desc, boolean default_) {
-        Property prop = ModuleLoader.config.get(category, propName, default_);
+        Property prop = configuration.get(category, propName, default_);
         prop.setComment(desc);
         setNeedsRestart(prop);
 
@@ -52,7 +58,7 @@ public class ModConfiguration {
     }
 
     public static String[] loadPropStringList(String propName, String category, String desc, String[] default_) {
-        Property prop = ModuleLoader.config.get(category, propName, default_);
+        Property prop = configuration.get(category, propName, default_);
         prop.setComment(desc);
         setNeedsRestart(prop);
         return prop.getStringList();
@@ -72,20 +78,29 @@ public class ModConfiguration {
 
     public void preInit(FMLPreInitializationEvent event)
     {
-        configuration = new Configuration(event.getSuggestedConfigurationFile());
+        configuration = new Configuration(event.getSuggestedConfigurationFile(),true);
+
         configuration.load();
 
-        configuration.getCategory("interaction").setComment("Enable or disable mod interactions.");
-        configuration.getCategory("addons").setComment("Configure individual addons.");
+        //configuration.getCategory("interaction").setComment("Enable or disable mod interactions.");
+        //configuration.getCategory("addons").setComment("Configure individual addons.");
 
+        MinecraftForge.EVENT_BUS.register(this);
+
+        loadModuleConfig();
+    }
+
+    public void loadModuleConfig()
+    {
         ModInteractions.setupConfig();
 
         if (configuration.hasChanged())
             configuration.save();
     }
 
-    public void init(FMLInitializationEvent event)
-    {
-
+    @SubscribeEvent
+    public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent eventArgs) {
+        if (eventArgs.getModID().equals(Reference.MOD_ID))
+            loadModuleConfig();
     }
 }
